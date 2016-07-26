ef39b7177921aad4f9f0c1353fbde7f74f3d32da
Celia Tomas <cici_927@hotmail.com>
2016-07-26 16:47:01 -0400

    commit message

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
+$(document).ready(function() {
 
-for(var i = 1;i < 101; i++){
-  if (i % 15 === 0) {
-     console.log('FizzBuzz');
-   } else if (i % 3 === 0) {
-       console.log('Fizz')
-   } else if (i % 5 === 0) {
-       console.log('Buzz') 
-   } else {
-          console.log(i)
-   }
+  function writeMessage(msg) {
+    $('#output').append(msg + '<br>');
+  }
 
-} 
+  function collectInfo() {
+    var num = prompt("Enter 1-100");
+    num = +num; //the "+" operator converts string to integer
+    //math.floor rounds decimal # down but if it's inside if block with (num) 
+    //and they do not equate to eachother, it renders false, making it go to collectInfo prompt again
+    if(Math.floor(num) == num && num > 0 && num < 101) {
+      fizzbuzz(num);
+    } 
+    else {
+      collectInfo()
+    }
+  }
+  collectInfo()
+
+  function fizzbuzz(n) {
+    for(i = 1; i <= n; i++) {
+      if(i % 15 == 0) {
+    	  writeMessage("FizzBuzz");
+      }
+      else if(i % 5 == 0) {
+    	  writeMessage("Buzz");
+      }
+      else if(i % 3 == 0) {
+    	  writeMessage("Fizz")
+      }
+      else {
+        writeMessage(i);
+      }
+    }
+  };
+});
